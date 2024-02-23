620118320aaa520930f7886c0f7ab42b110ca62e
amit87000 <amit.kaushik@ayasya.com>
2024-02-23 18:01:00 +0530

    update tracker app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import Tracker from './components/Tracker';
 import './App.css';
+import styled from 'styled-components';
+import GlobalStyles from './components/globalStyles';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Expense Tracker</h2>
+
+      <Tracker />
+      <GlobalStyles />
     </div>
   );
 }
